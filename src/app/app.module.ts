2e3953b7716174ada5972c5ebc3401c0d237271e
anailson <arsantos315@gmail.com>
2024-01-07 16:29:35 -0300

    Projeto Redux Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarUsuariosComponent } from './listar-usuarios/listar-usuarios.component';
+import { MainComponent } from './main/main.component';
+import { CadastroUsuariosComponent } from './cadastro-usuarios/cadastro-usuarios.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Adicione essas linhas para importar os módulos do Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarUsuariosComponent,
+    MainComponent,
+    CadastroUsuariosComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../Models/UsuarioModel';
+import { UsuariosService } from '../Repository/UsuariosService';
+
+@Component({
+  selector: 'app-cadastro-usuarios',
+  templateUrl: './cadastro-usuarios.component.html',
+  styleUrls: ['./cadastro-usuarios.component.scss']
+})
+export class CadastroUsuariosComponent implements OnInit {
+
+  model: UsuarioModel = {id:0, nome:'', idade:0, perfil:''};
+  constructor(private usuarioService: UsuariosService) { }
+
+  ngOnInit(): void {
+  }
+  addUsuario(){
+   if(this.model.id == 0){
+    console.log('cadastro',this.model)
+    //cadastro
+    this.usuarioService.addUsuario(this.model).subscribe();
+   }else{
+    console.log('atualizar',this.model)
+//atualizar
+   }
+
+  }
+
+}
